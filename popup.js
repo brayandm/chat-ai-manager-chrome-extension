834722e51c02a4191ed4eb899df24ce4de98d4ad
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-02-14 17:20:39 +0100

    Add Whatsapp link to manager.html and create event listener in popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,7 @@
 console.log("This is a popup!");
+
+document
+    .getElementById("whatsapp-web-link")
+    .addEventListener("click", function () {
+        chrome.tabs.create({ url: "https://web.whatsapp.com/" });
+    });
